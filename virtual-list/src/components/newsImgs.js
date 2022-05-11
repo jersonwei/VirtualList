a401812ca02a4db646b6a91a100b882119264b1e
王伟 <872937094@qq.com>
2022-05-11 23:01:54 +0800

    static pages create/fork mockdata

---
+++ virtual-list/src/components/newsImgs.js
@@ -0,0 +1,29 @@
+import img1 from '../assets/news/1.webp'
+import img2 from '../assets/news/2.webp'
+import img3 from '../assets/news/3.webp'
+import img4 from '../assets/news/4.webp'
+import img5 from '../assets/news/5.webp'
+import img6 from '../assets/news/6.webp'
+import img7 from '../assets/news/7.webp'
+import img8 from '../assets/news/8.webp'
+import img9 from '../assets/news/9.webp'
+import img10 from '../assets/news/10.webp'
+import img11 from '../assets/news/11.webp'
+import img12 from '../assets/news/12.webp'
+
+let imgsList = [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12
+]
+
+export default imgsList
---
+++ virtual-list/src/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="news-box"></div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+<style lang="less"></style>
