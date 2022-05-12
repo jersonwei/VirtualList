2a0134c6f3de75561eb629b12db3301d8b78f67c
王伟 <872937094@qq.com>
2022-05-12 13:57:21 +0800

    pass some extracted data from the parent component

--- virtual-list/src/pages/index.vue
+++ virtual-list/src/pages/index.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="news-box">
-    <VituralScroll></VituralScroll>
+    <VituralScroll
+      :msg="msg"
+      :oneHeight="oneHeight"
+      :requestUrl="requestUrl"
+      :requestNum="requestNum"
+    ></VituralScroll>
   </div>
 </template>
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      // 请求数据提示信息
+      msg: '正在努力加载信息,请稍候',
+      // 记录单条数据的高度
+      oneHeight: 150,
+      // 请求数据的Url
+      requestUrl: 'http://localhost:4020/data?num=',
+      // 请求数据的条数
+      requestNum: 30
+    }
+  }
+}
+</script>
 <style lang="less">
 .news-box {
   width: 100%;
